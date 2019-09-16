f2136acd01b387cb61a7cfdfc5df24f34b1cd916
AbSamad99 <abdussamad.mi5@gmail.com>
2019-09-16 18:10:15 +0530

    deploy

--- src/components/Text.js
+++ src/components/Text.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Text=({id,log}) => {
   return(
     <div className='tc items-center flex justify-center flex-column'>
-    <h2>{`Log #${id}`}</h2>
+    <h2>{`Log #${id+1}`}</h2>
     <div id='text'>
       {log}
     </div>
